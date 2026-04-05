a3be7aa796f5584f57ccb3ffd915db5e86a74a84
xiaoyu <xiaoyu@sulg.top>
2026-04-06 02:02:09 +0800

    🐛 修复alternates异常问题

--- src/components/BaizeToolbox/BaizeToolboxInfo.tsx
+++ src/components/BaizeToolbox/BaizeToolboxInfo.tsx
@@ -3,15 +3,11 @@ import { repoUrl, baseParams } from "@/utils/githubHelper";
 import PageInfo from "../Common/PageInfo";
 import Download from "./download";
 import { gitHubUrl } from "@/utils/textHelper";
-import { Metadata } from "next";
 
-export const metaData: Metadata = {
+export const data = {
   title: "白泽工具箱",
   description:
     "白泽工具箱是一款功能强大的多媒体工具，为用户提供了多种多样的多媒体处理功能。无论您是需要对音视频转码、提取音频/视频/字幕、文字转语音(TTS)、屏幕录制、截图，还是对音视频图片进行压缩，这个工具都能轻松实现。",
-  alternates: { canonical: "/baize-toolbox" },
-  keywords:
-    "白泽工具箱, 多媒体处理工具, 音视频转码软件, 视频剪辑, 文字转语音, 屏幕录制软件, 截图工具, 音视频图片压缩",
 };
 export const dynamic = "force-dynamic";
 
@@ -33,7 +29,7 @@ export default async function BaizeToolboxInfo() {
 
   return (
     <div className="container">
-      <PageInfo title={metaData?.title} description={metaData?.description} />
+      <PageInfo title={data?.title} description={data?.description} />
 
       {/* <AppGithubStatus repoInfo={repoInfo} contributors={contributors} /> */}
 
